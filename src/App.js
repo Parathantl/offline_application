8ba675d49abe4aa20d1549316e8a9dbf7fe581e8
parathantl <parathanlive123@gmail.com>
2021-12-25 01:21:44 +0530

    Audio Player Integration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Grade from './components/Grade'
 import Grade1 from './components/Grade1'
 import PDFReader from './components/pdfcomponents/PDFReader';
+import Audio from './components/audio'
 
 import { history } from './history';
 import { BrowserRouter  as Router, Routes, Route } from "react-router-dom";
@@ -9,9 +10,13 @@ import { BrowserRouter  as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <div style={{marginTop: 20}}>
+      Nav bar Position
+      </div>
         <Router history={history}>
           <Routes>
-            <Route path="/check" element={<PDFReader/>}/>
+            <Route path="/pdf" element={<PDFReader/>}/>
+            <Route path="/audio" element={<Audio/>}/>
             <Route path="/grade1" element={<Grade1/>}/>
             <Route exact path="/" element={<Grade/>} />
           </Routes>
---
+++ src/components/audio.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+
+export default function audio() {
+
+    
+    return (
+        <div>
+            <audio controls>
+            <source src="https://assets.coderrocketfuel.com/pomodoro-times-up.mp3" type="audio/mpeg">
+                </source>
+            </audio>
+        </div>
+    )
+}
